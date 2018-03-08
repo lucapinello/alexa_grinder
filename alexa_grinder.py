50f55c1437d10aa391c09461226cfdb6bf3f3348
Luca Pinello <lucapinello@gmail.com>
2018-03-07 22:54:02 -0500

    settings page and json apps setting

--- alexa_grinder.py
+++ alexa_grinder.py
@@ -1,12 +1,16 @@
 import logging
 import subprocess as sb
-from flask import Flask, render_template, request
+from flask import Flask, render_template, Response, request, redirect, url_for
 
 from flask_ask import Ask, statement, question, session
 
-DURATION_SINGLE=8.3
-DURATION_DOUBLE=15.4
-HS100_IP='192.168.0.115'
+import json
+
+app_settings = json.load(open('app_settings.json'))
+
+DURATION_SINGLE=app_settings['DURATION_SINGLE']
+DURATION_DOUBLE=app_settings['DURATION_DOUBLE']
+HS100_IP=app_settings['HS100_IP']
 
 app = Flask(__name__)
 
@@ -41,6 +45,26 @@ def double():
     grind_coffee('double')
     #sb.Popen('python grinder_control.py -t %s -d %f' % (HS100_IP,DURATION_DOUBLE),shell=True)
     return statement(msg)
+
+@app.route("/settings",methods = ['GET', 'POST'])
+def settings():
+    global DURATION_SINGLE
+    global DURATION_DOUBLE	
+    global HS100_IP
+    if request.method == 'GET':	
+    	return render_template('index.html',
+				singleDuration=DURATION_SINGLE,
+				doubleDuration=DURATION_DOUBLE,
+				HS100_IP=HS100_IP)
+    elif request.method == 'POST':
+	DURATION_SINGLE=float(request.form['singleDuration'])
+	DURATION_DOUBLE=float(request.form['doubleDuration'])
+	HS100_IP=request.form['HS100_IP']
+	with open('app_settings.json', 'w+') as fp:
+    		json.dump({'DURATION_SINGLE':DURATION_SINGLE,
+			   'DURATION_DOUBLE':DURATION_DOUBLE,
+			   'HS100_IP':HS100_IP}, fp)
+	return redirect(url_for('settings'))
     
 @app.route('/grind/<quantity>', methods = ['GET', 'POST'])
 def iftt_grind(quantity):
